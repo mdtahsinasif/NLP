b9c1ec27791a439d2f99c7529b03497a1296cc02
Md Tahsin Asif <32189971+mdtahsinasif@users.noreply.github.com>
2020-06-16 15:11:03 +0530

    Add files via upload

--- DarkWebScrapper/DarkWebScrapper.py
+++ DarkWebScrapper/DarkWebScrapper.py
@@ -1,89 +1,305 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 28 19:25:06 2020
-
-@author: tahsin.asif
-"""
-
+import requests
+from bs4 import BeautifulSoup
+import pymongo
+import re
+from pymongo import MongoClient
+# import MySQLdb
+import datetime
+import logging
+from nltk.tokenize import word_tokenize, sent_tokenize
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-import pandas as pd
-
-binary = FirefoxBinary('..../Tor Browser/Browser/firefox')
-service_args = [ '--proxy=localhost:9150', '--proxy-type=socks5', ]
-
-driver = webdriver.PhantomJS(service_args=service_args)
-#get page
-#driver.get("https://stackoverflow.com/questions/40161921/how-to-open-and-close-tor-browser-automatically-with-python")
-print(driver.page_source)
-
-##for captcha reading
-url = "...."
-driver.get(url)
-driver.implicitly_wait(10000)
-#pip install cssselect
-#pip install lxml
-import lxml.html
-import urllib.request as urllib2
-import pprint
-import http.cookiejar as cookielib
-def form_parsing(html):
-   tree = lxml.html.fromstring(html)
-   data = {}
-   for e in tree.cssselect('form input'):
-      if e.get('name'):
-         data[e.get('name')] = e.get('value')
-   return data
-REGISTER_URL = '.'
-ckj = cookielib.CookieJar()
-browser = urllib2.build_opener(urllib2.HTTPCookieProcessor(ckj))
-driver.get(url)
-html = (driver.find_element_by_xpath('/html').text)
-print(html)
-html = browser.open('http://avengersdutyk3xf.onion/').read()
-form = form_parsing(html)
-pprint.pprint(form)
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 
+class SoupScraper():
+    # client = MongoClient('mongodb://appmongouser:XWDXJd2WIc3MFGI8@prod-shard-00-00-agp94.mongodb.net:27017,prod-shard-00-01-agp94.mongodb.net:27017,prod-shard-00-02-agp94.mongodb.net:27017,prod-shard-00-03-agp94.mongodb.net:27017,prod-shard-00-04-agp94.mongodb.net:27017,prod-shard-00-05-agp94.mongodb.net:27017,prod-shard-00-06-agp94.mongodb.net:27017/test?ssl=true&replicaSet=Prod-shard-0&authSource=admin&retryWrites=true&w=majority&readPreference=nearest')
+    # db = client.core
+    # collection = db.dark_web_search
+    logging.basicConfig(filename="SoupScraper.log",
+                        format='%(asctime)s %(message)s',
+                        filemode='w')
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
 
-#######
-#driver = webdriver.Firefox(firefox_binary = binary)
-#url = "http://suprbayoubiexnmp.onion/"
-org_name = 'Mitsubushi'
-url = 'http://searchcoaupi3csb.onion/search/?q=' + org_name + '&ex_q=timestamp%3A[now%2Fd-1d+TO+*]&sdh=&'
-print(url)
-driver.get(url)
-#print(driver.find_element_by_class_name("mw-redirect").text)
-print(driver.find_element_by_xpath('/html').text)
-driver.find_elements_by_class_name("postMain")
-driver.find_element_by_class_name('postContent').text
-post_content_list = []
-#postText = str(driver.find_element_by_class_name('mw-redirect').text)
-postText = str(driver.find_element_by_xpath('/html').text)
-print(postText)
+    def count_page(self, org_name):
+        try:
+            url = org_name + "&num=10&sort="
+           # print("count_page:", url)
+            driver.get(url)
+        except requests.Timeout:
+            self.logger.info('Timed out')
+            return
+        page = driver.page_source
+        soup = BeautifulSoup(page, 'html.parser')
+        #print(soup)
+        paging = soup.find("div", {"class": "row", "id": ""}).find("ul", {"class": "pagination"}).find_all("a")
+    #    print(paging)
+        start_page = paging[1].text
+     #   print("Start Page", start_page)
+        last_page = paging[len(paging) - 2].text
+      #  print("Last Page", last_page)
+        return last_page
 
-from bs4 import BeautifulSoup
-html = postText
-soup = BeautifulSoup(html, "html.parser")
-soup.findAll('a')
-one_a_tag = soup.findAll("a")[36]
-link = one_a_tag["href"]
+    def getLinks(self, domain, org_name):
+
+        if domain == ("http://searchcoaupi3csb.onion/search/"):
+            url = "http://searchcoaupi3csb.onion/search/?q=" + org_name + "&num=10&sort="
+            try:
+                counter = scraper.count_page(url)
+            except requests.Timeout:
+                self.logger.info('Timed out')
+                return
+
+        else:
+            url = domain
+          #  counter = scraper.count_page(url)
+
+        self.logger.info('Starting getLinks for ' + org_name)
+        if (domain == ("http://searchcoaupi3csb.onion/search/")):
+           # for x in range(int(counter)):
+            for x in range(int(counter)):
+                if x == 0:
+                    url = "http://searchcoaupi3csb.onion/search/?q=" + org_name + "&num=10&sort="
+                  #  print("IFURL:", url)
+                else:
+                  #  url = "http://searchcoaupi3csb.onion/search/move/?q=vulnerability&pn=1&num=10&sdh=&"
+                    url = "http://searchcoaupi3csb.onion/search/move/?q=" + org_name + "&pn=" + str(x + 1) + "&num=10&sdh=&"
+                  #  print("ElseURL:", url)
+
+                try:
+                    driver.get(url)
+            #        homePageText = driver.find_element_by_xpath('/html').text
+             #       print('homePageText::::->',homePageText)
+              #      result =  scraper.run_summarization(homePageText)
+               #     keys = domain
+                #    dw[keys] = result
+                except requests.Timeout:
+                    self.logger.info('Timed out')
+                    return
+                page = driver.page_source
+                soup = BeautifulSoup(page, 'html.parser')
+                self.logger.info('Org:' + str(org_name))
+                #        for div in soup.find_all("cite"):
+                #            print(div)
+                for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
+                    self.logger.info(link['href'])
+                    query = {"url": link['href']}
+                    #    print("Links::->",link['href'] )
+                    try:
+                        driver.get(link['href'])
+                        linkPageText = driver.find_element_by_xpath('/html').text
+                        linkSummary = scraper.run_summarization(linkPageText)
+                        # print(linkSummary)
+                        keys = link['href'].text
+                        dw[keys] = linkSummary
+                    except:
+                        continue
+
+             #   print("LowerCounter::", x)
+                print("Length of Dictionary:::", len(dw))
+        else:
+                try:
+                    driver.get(url)
+            #        homePageText = driver.find_element_by_xpath('/html').text
+             #       print('homePageText::::->',homePageText)
+              #      result =  scraper.run_summarization(homePageText)
+               #     keys = domain
+                #    dw[keys] = result
+                except requests.Timeout:
+                    self.logger.info('Timed out')
+                    return
+                page = driver.page_source
+                soup = BeautifulSoup(page, 'html.parser')
+                self.logger.info('Org:' + str(org_name))
+                #        for div in soup.find_all("cite"):
+                #            print(div)
+                for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
+                    self.logger.info(link['href'])
+                    query = {"url": link['href']}
+                    #    print("Links::->",link['href'] )
+                    try:
+                        driver.get(link['href'])
+                        linkPageText = driver.find_element_by_xpath('/html').text
+                        linkSummary = scraper.run_summarization(linkPageText)
+                        # print(linkSummary)
+                        keys = link['href']
+                        dw[keys] = linkSummary
+                    except:
+                        continue
+                print("Length of Dictionary:::", len(dw))
+
+        #            [x.extract() for x in pg_soup.findAll(['script', 'style', 'nonscript'])]
+        #            [y.decompose() for y in pg_soup.findAll(['span', 'li', 'noscript', 'footer',
+        #                                              'title', 'a','h3' ])]
+
+        #   dw['org_id'] = org_id
+        # dw['org_name'] = org_name
+        #  dw['asset'] = asset_type
+
+        # dw['content'] =linkSummary
+        # print("Link:::-"+link['href']+"==================="+"Content:::-"+pg_soup.get_text())
+        # dw['created_date'] = datetime.datetime.utcnow()
+
+        #  self.collection.insert_one(dw)
+        # self.client.close()
+
+    #    def getOrgs(self):
+    #        self.logger.info('Starting getOrgs')
+    #        db = MySQLdb.connect("jp-rds01.c7tohwlgi9s5.ap-northeast-1.rds.amazonaws.com","cyorguser","bkTQAp76YU29ZD45dBJ5CjvN","org")
+    #        cursor = db.cursor()
+    #        cursor.execute("SELECT * FROM org where status='Active'")
+    #        numrows = cursor.rowcount
+    #        for x in xrange(0,numrows):
+    #            row = cursor.fetchone()
+    #            self.getLinks(row[1], row[0], 'Company')
+    #        db.close()
+
+    def _create_frequency_table(self, text_string) -> dict:
+        """
+        we create a dictionary for the word frequency table.
+        For this, we should only use the words that are not part of the stopWords array.
+        Removing stop words and making frequency table
+        Stemmer - an algorithm to bring words to its root word.
+        :rtype: dict
+        """
+      #  print('Inside Frequenct Table')
+        stopWords = set(stopwords.words("english"))
+        # adding beautiful soap logic to get clean text from html
+
+        words = word_tokenize(text_string)
+        ps = PorterStemmer()
+
+        freqTable = dict()
+        for word in words:
+            word = ps.stem(word)
+            if word in stopWords:
+                continue
+            if word in freqTable:
+                freqTable[word] += 1
+            else:
+                freqTable[word] = 1
+
+        return freqTable
+
+    def _score_sentences(self, sentences, freqTable) -> dict:
+        """
+        score a sentence by its words
+        Basic algorithm: adding the frequency of every non-stop word in a sentence divided by total no of words in a sentence.
+        :rtype: dict
+        """
+
+        sentenceValue = dict()
+
+        for sentence in sentences:
+            word_count_in_sentence = (len(word_tokenize(sentence)))
+            word_count_in_sentence_except_stop_words = 0
+            for wordValue in freqTable:
+                if wordValue in sentence.lower():
+                    word_count_in_sentence_except_stop_words += 1
+                    if sentence[:10] in sentenceValue:
+                        sentenceValue[sentence[:10]] += freqTable[wordValue]
+                    else:
+                        sentenceValue[sentence[:10]] = freqTable[wordValue]
+
+            if sentence[:10] in sentenceValue:
+                sentenceValue[sentence[:10]] = sentenceValue[sentence[:10]] / word_count_in_sentence_except_stop_words
+
+            '''
+            Notice that a potential issue with our score algorithm is that long sentences will have an advantage over short sentences. 
+            To solve this, we're dividing every sentence score by the number of words in the sentence.
+
+            Note that here sentence[:10] is the first 10 character of any sentence, this is to save memory while saving keys of
+            the dictionary.
+            '''
+
+        return sentenceValue
+
+    def _find_average_score(self, sentenceValue) -> int:
+        """
+        Find the average score from the sentence value dictionary
+        :rtype: int
+        """
+        global average
+        sumValues = 0
+
+        for entry in sentenceValue:
+            sumValues += sentenceValue[entry]
+        try:
+            # Average value of a sentence from original text
+            average = (sumValues / len(sentenceValue))
+
+        except ZeroDivisionError:
+            print("Description is Empty:::Phising Webpage!")
+
+        return average
+
+    def _generate_summary(self, sentences, sentenceValue, threshold):
+        sentence_count = 0
+        summary = ''
+
+        for sentence in sentences:
+            if sentence[:10] in sentenceValue and sentenceValue[sentence[:10]] >= (threshold):
+                summary += " " + sentence
+                sentence_count += 1
+
+        return summary
+
+    def run_summarization(self, text):
+        # print('Inside Review Text',text)
+        # 1 Create the word frequency table
+        freq_table = scraper._create_frequency_table(text)
+        # print('Inside freq_table------------',)
+        '''
+        We already have a sentence tokenizer, so we just need 
+        to run the sent_tokenize() method to create the array of sentences.
+        '''
+
+        # 2 Tokenize the sentences
+        sentences = sent_tokenize(text)
+
+        # 3 Important Algorithm: score the sentences
+        sentence_scores = scraper._score_sentences(sentences, freq_table)
+
+        # 4 Find the threshold
+        threshold = scraper._find_average_score(sentence_scores)
+
+        # 5 Important Algorithm: Generate the summary
+        summary = scraper._generate_summary(sentences, sentence_scores, 0.8 * threshold)
+        return summary
+
+
+binary = FirefoxBinary('C:/Users/tahsin.asif/Desktop/Tor Browser/Browser/firefox')
+service_args = ['--proxy=localhost:9150', '--proxy-type=socks5', ]
+dataDic = {}
+
+
+
+# scraper.getOrgs()
+
+if __name__ =='__main__':
+    driver = webdriver.PhantomJS(service_args=service_args)
+    scraper = SoupScraper()
+    dw = {}
+    org_name = "Vulnerablity"
+    from csv import reader
+
+    # open file in read mode
+    with open('DarkWebCSVInput.csv',
+              'r') as read_obj:
+        # pass the file object to reader() to get the reader object
+        csv_reader = reader(read_obj)
+        # Iterate over each row in the csv using reader object
+        for row in csv_reader:
+            # row variable is a list that represents a row in csv
+           # print(str(row))
+            domainName = ''.join(map(str,row))
+            scraper.getLinks(domainName,org_name)
+            for k, v in dw.items():
+                print("Keys::------",k)
+                print("Values::----",v)
 
 
 
 
-print(post_content_list.append(driver.find_element_by_xpath('/html').text))
-
-for i in range(1, MAX_PAGE_NUM + 1):
-  page_num = i
-  url = '*first part of url*' + str(page_num) + '*last part of url*'
-  driver.get(url)
-  
-df['postURL'] = post_url_list
-df['author'] = post_author_list
-df['postTitle'] = post_title_list
-df.to_csv('scrape.csv')
-
-  
-
-     
